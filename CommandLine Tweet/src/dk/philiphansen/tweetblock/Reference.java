0124b8ef73c88d0e1498ee967389dbeb6b984f52
Philip Hansen <me@philiphansen.dk>
2014-01-31 18:27:57 +0100

    Tweet @user
    Program generates authorization url, upon authorizing the app the
    @MinecraftTweetBlock user will tweet "@user Hi"

---
+++ CommandLine Tweet/src/dk/philiphansen/tweetblock/Reference.java
@@ -0,0 +1,8 @@
+package dk.philiphansen.tweetblock;
+
+public final class Reference {
+	protected static final String CONSUMER_KEY = "XXX";
+	protected static final String CONSUMER_SECRET = "XXX";
+	protected static final String MINECRAFTTWEETBLOCK_TOKEN = "XXX";
+	protected static final String MINECRAFTTWEETBLOCK_SECRET = "XXX";
+}
