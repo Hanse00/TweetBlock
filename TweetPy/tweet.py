7ba73231eca4e73316df80f8816820c5dea37691
Philip Hansen <me@philiphansen.dk>
2014-02-01 14:14:35 +0100

    Tweet at authorizing user
    Implemented full functionality of CommandLine Tweet. The script now
    generates a twitter authorization link, upon user authorization, the
    username is grabbed.
    Username concatenated with input string is then tweeted by TweetBlock
    account.

--- TweetPy/tweet.py
+++ TweetPy/tweet.py
@@ -1,16 +1,42 @@
-import tweepy
+import tweepy, traceback
 
 consumer_key = "XXX"
 consumer_secret = "XXX"
 
-access_token = "XXX"
-access_token_secret = "XXX"
+tweetblock_access_token = "XXX"
+tweetblock_access_token_secret = "XXX"
+
+user_access_token = ""
+
+message = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+request = auth.get_authorization_url()
 
-print api.me().name
+while user_access_token == "":
+    print "Go to the following URL to authorize TweetBlock:",
+    print request
+    pin = raw_input("Enter pin: ")
 
-api.update_status("Python?")
+    try:
+        user_access_token = auth.get_access_token(pin)
+    except TweepError:
+        traceback.print_exc()
+
+user_api = tweepy.API(auth)
+
+username = user_api.me().screen_name
+print username
+
+while message == "":
+    print "What would you like me to tweet you master?"
+    message = raw_input("Message: ")
+
+auth.set_access_token(tweetblock_access_token, tweetblock_access_token_secret)
+tweetblock_api = tweepy.API(auth)
+
+status = "@" + username + " " + message
+tweetblock_api.update_status(status)
+print status
+        
