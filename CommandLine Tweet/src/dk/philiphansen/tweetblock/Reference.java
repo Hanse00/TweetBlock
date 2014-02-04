cf578419f95f032e71cddf535fb55ba1684c6656
Philip Hansen <me@philiphansen.dk>
2014-02-04 21:12:59 +0100

    Cleaned old java code out of python branch

--- CommandLine Tweet/src/dk/philiphansen/tweetblock/Reference.java
+++
@@ -1,8 +0,0 @@
-package dk.philiphansen.tweetblock;
-
-public final class Reference {
-	protected static final String CONSUMER_KEY = "XXX";
-	protected static final String CONSUMER_SECRET = "XXX";
-	protected static final String MINECRAFTTWEETBLOCK_TOKEN = "XXX";
-	protected static final String MINECRAFTTWEETBLOCK_SECRET = "XXX";
-}
