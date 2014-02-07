bc0dcbd2c4e1c0a25cb00e26c989abc74b095930
Philip Hansen <me@philiphansen.dk>
2014-02-07 23:35:17 +0100

    Preparation for for block functionality
    Added basic tile entity and converted the block to work with that tile
    entity, in preparation for the tweet functionality that will require a
    tile entity.

--- TweetBlock/src/hanse00/tweetblock/blocks/BlockTweetBlock.java
+++ TweetBlock/src/hanse00/tweetblock/blocks/BlockTweetBlock.java
@@ -1,14 +1,20 @@
 package hanse00.tweetblock.blocks;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import hanse00.tweetblock.reference.BlockInfo;
-import net.minecraft.block.Block;
+import hanse00.tweetblock.tileentities.TileEntityTweetBlock;
+
+import java.util.Random;
+
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
-public class BlockTweetBlock extends Block {
+public class BlockTweetBlock extends BlockContainer {
 	
 	public BlockTweetBlock(int id) {
 		super(id, Material.iron);
@@ -22,6 +28,16 @@ public class BlockTweetBlock extends Block {
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IconRegister register) {
 		blockIcon = register.registerIcon(BlockInfo.TEXTURE_LOCATION + ":" + BlockInfo.TWEETBLOCK_TEXTURE);
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World world) {
+		return new TileEntityTweetBlock();
+	}
+	
+	@Override
+	public int idDropped(int meta, Random random, int fortune) {
+		return -1;
 	}
 
 }
---
+++ TweetBlock/src/hanse00/tweetblock/tileentities/TileEntityTweetBlock.java
@@ -0,0 +1,21 @@
+package hanse00.tweetblock.tileentities;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityTweetBlock extends TileEntity {
+	
+	public TileEntityTweetBlock() {
+
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+	}
+}
