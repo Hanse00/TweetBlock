8cff7f02b9843d9d3a99529864a7f5919870860e
Philip Hansen <me@philiphansen.dk>
2014-02-09 21:13:14 +0100

    Move twtter info to local files
    Previously used --assume-unchanged on info files, now moved to having
    actual keys in local ignored files.

--- TweetBlock/src/hanse00/tweetblock/reference/TweetInfo.java
+++ TweetBlock/src/hanse00/tweetblock/reference/TweetInfo.java
@@ -1,12 +1,12 @@
 package hanse00.tweetblock.reference;
 
 public class TweetInfo {
-	public static final String GET_CONSUMER_KEY = "XXX";
-	public static final String GET_CONSUMER_SECRET = "XXX";
+	public static final String	ACCESS_SECRET			= "XXX";
+	public static final String	ACCESS_TOKEN			= "XXX";
 
-	public static final String POST_CONSUMER_KEY = "XXX";
-	public static final String POST_CONSUMER_SECRET = "XXX";
-	
-	public static final String ACCESS_TOKEN = "XXX";
-	public static final String ACCESS_SECRET = "XXX";
+	public static final String	GET_CONSUMER_KEY		= "XXX";
+	public static final String	GET_CONSUMER_SECRET		= "XXX";
+
+	public static final String	POST_CONSUMER_KEY		= "XXX";
+	public static final String	POST_CONSUMER_SECRET	= "XXX";
 }
